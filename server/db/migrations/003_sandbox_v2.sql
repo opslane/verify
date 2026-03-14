e74a5b50c3816d2015a697a5fbd66c0b1c769169
Abhishek Ray <abhishekray07@gmail.com>
2026-03-14 07:48:07 -0700

    fix: wrap migration 003 in transaction, add entrypoint timeout
    - Migration: BEGIN/COMMIT ensures atomic schema changes
    - Entrypoint: 30s timeout on Docker daemon readiness (fail fast vs hang)

--- server/db/migrations/003_sandbox_v2.sql
+++ server/db/migrations/003_sandbox_v2.sql
@@ -4,6 +4,8 @@
 -- Replaces pre_start_script with schema_command + seed_command
 -- Adds login_script, sandbox_template
 
+BEGIN;
+
 ALTER TABLE repo_configs RENAME COLUMN startup_command TO dev_command;
 
 ALTER TABLE repo_configs DROP COLUMN detected_infra;
@@ -14,3 +16,5 @@ ALTER TABLE repo_configs ADD COLUMN schema_command text;
 ALTER TABLE repo_configs ADD COLUMN seed_command text;
 ALTER TABLE repo_configs ADD COLUMN login_script text;
 ALTER TABLE repo_configs ADD COLUMN sandbox_template text;
+
+COMMIT;
